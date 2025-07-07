76e76cac6e5a132a198f931d019a764d6879d9b4
chanchal430 <kchanchal430@gmail.com>
2025-07-07 18:12:10 +0530

    added text from Colors

--- components/common/CategoryCard.tsx
+++ components/common/CategoryCard.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import Colors from '@/theme/Colors';
-import styles from './styles/CategoryCard.module.css';
-import { Text } from '@telegram-apps/telegram-ui';
-
+import React from "react";
+import Colors from "@/theme/Colors";
+import styles from "./styles/CategoryCard.module.css";
+import { Text } from "@telegram-apps/telegram-ui";
 
 interface CardProps {
-    icon: React.ElementType;
-    title?: string;
-    onClick?: () => void;
-    className?: string;
-    type?: 'round' | 'square' | 'rectangle';
-    size?: 'x-small' | 'small' | 'medium' | 'large';
+  icon: React.ElementType;
+  title?: string;
+  onClick?: () => void;
+  className?: string;
+  type?: "round" | "square" | "rectangle";
+  size?: "x-small" | "small" | "medium" | "large";
 }
 
-const CategoryCard: React.FC<CardProps> = ({ icon: Icon, title, onClick, className, type = 'square', size = 'medium' }) => {
-    const cardClass = `${styles.card} ${styles[type]} ${styles[size]} ${className || ''}`;
+const CategoryCard: React.FC<CardProps> = ({
+  icon: Icon,
+  title,
+  onClick,
+  className,
+  type = "square",
+  size = "medium",
+}) => {
+  const cardClass = `${styles.card} ${styles[type]} ${styles[size]} ${
+    className || ""
+  }`;
 
-    return (
-        <div className={cardClass} onClick={onClick} style={{ backgroundColor: Colors.primaryBlack }}>
-            {/* <Icon className={styles.icon}/> */}
-            <Text weight='3' style={{color: Colors.text}}>{title}</Text>
-        </div>
-    );
+  return (
+    <div
+      className={cardClass}
+      onClick={onClick}
+      style={{ backgroundColor: Colors.dark.base[100].DEFAULT }}
+    >
+      {/* <Icon className={styles.icon}/> */}
+      <Text weight="3" style={{ color: Colors.dark.base.content.DEFAULT }}>
+        {title || ""}
+      </Text>
+    </div>
+  );
 };
 
 export default CategoryCard;
